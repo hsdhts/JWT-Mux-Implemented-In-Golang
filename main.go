452a872a54bcd368890e68a1c0374fe6d22aaa91
husada20 <husadahutasoit@gmail.com>
2023-08-27 23:00:19 +0700

    success hashing pw

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"github.com/gorilla/mux"
 	"github.com/jeypc/go-jwt-mux/controllers/authcontroller"
+	"github.com/jeypc/go-jwt-mux/models"
 	"log"
 	"net/http"
 )
 
 func main() {
 	r := mux.NewRouter()
+	models.ConnectDatabase()
 
 	r.HandleFunc("/login", authcontroller.Login).Methods("POST")
 	r.HandleFunc("/register", authcontroller.Register).Methods("POST")
