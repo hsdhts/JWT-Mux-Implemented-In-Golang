01a7db8528ff677520eef1411ed962060711a9b8
husada20 <husadahutasoit@gmail.com>
2023-08-27 22:54:06 +0700

    resolved authcontroller dir

--- controllers/authcontroller/authcontroller.go
+++ controllers/authcontroller/authcontroller.go
@@ -9,31 +9,31 @@ import (
 )
 
 func Login(w http.ResponseWriter, r *http.Request) {
-	// mengambil inputan json
-	var userInput models.User
-	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&userInput); err != nil {
-		log.Fatal("Gagal mendecode JSON")
-	}
 
-	defer r.Body.Close()
-
-	// hash password menggunakan bcrypt
-	hashPassword, _ := bcrypt.GenerateFromPassword([]byte(userInput.Password), bcrypt.DefaultCost)
-	userInput.Password = string(hashPassword)
-
-	//insert ke database
-	if err := models.DB.Create(&userInput).Error; err != nil {
-		log.Fatal("Gagal menyimpan Data")
-	}
-
-	response, _ := json.Marshal(map[string]string{"message": "success"})
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(response)
 }
 
 func Register(w http.ResponseWriter, r *http.Request) {
+	// mengambil inputan json dari client
+	var userInput models.User
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&userInput); err != nil {
+		log.Fatal("Gagal mendecode json")
+	}
+	defer r.Body.Close()
+
+	//hashing password with bcrypt
+	hashPassword, _ := bcrypt.GenerateFromPassword([]byte(userInput.Password), bcrypt.DefaultCost)
+	userInput.Password = string(hashPassword)
+
+	//insert to database
+	if err := models.DB.Create(&userInput).Error; err != nil {
+		log.Fatal("gagal menyimpan data")
+	}
+	response, _ := json.Marshal(map[string]string{"message": "success"})
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
 
 }
 
